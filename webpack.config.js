4ca3d16686568c8a963e88b45fb995f8c95beed5
Pavel <bavenka@gmail.com>
2017-06-21 14:30:03 +0300

    added dependencies and tools to start development

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, loaders: ["react-hot-loader", "babel-loader"] }
+        ]
+    }
+};
