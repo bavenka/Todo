a2c209ea21830e8b514eb0ae558feaef7daa81a3
MIKL99999 <radykevich@gmail.com>
2017-06-24 22:43:34 +0300

    Change event onClick to onChange.

--- Frontend/src/components/completeAll.js
+++ Frontend/src/components/completeAll.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const CompleteAll = ({check, onCompleteAllClick}) => (
-    <input type="checkbox" checked={check} onClick={onCompleteAllClick}/>
+    <input type="checkbox" checked={check} onChange={onCompleteAllClick}/>
 );
 
 export default CompleteAll
