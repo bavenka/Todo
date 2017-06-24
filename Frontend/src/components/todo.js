f0bd123dbea59be0a64d8e29e480288a01e400b5
MIKL99999 <radykevich@gmail.com>
2017-06-24 20:53:56 +0300

    Fix bug( unchecked checkbox ).

--- Frontend/src/components/todo.js
+++ Frontend/src/components/todo.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Todo = ({text, onDeleteClick, onCompleteClick, completed}) => (
     <li style={{textDecoration: completed ? 'line-through' : 'none'}}>
-        <input type="checkbox" onClick={onCompleteClick}/>
+        <input type="checkbox" checked={completed} onClick={onCompleteClick}/>
         {text}
         <button onClick={onDeleteClick}>x</button>
     </li>
