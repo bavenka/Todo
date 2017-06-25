46766e790dd7ad57c9b11f2b6da1501f7b5e1fe1
Pavel <bavenka@gmail.com>
2017-06-25 13:32:24 +0300

    fixed focus work

--- Frontend/src/components/header.js
+++ Frontend/src/components/header.js
@@ -25,7 +25,8 @@ let Header = ({dispatch}) => {
         <div>
             <CompleteAll/>
             <input ref={node => {
-                textInput = node && node.focus()
+                textInput = node;
+                node.focus()
             }} onKeyDown={handleNewTodoKeyDown}/>
         </div>
     )
