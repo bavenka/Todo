f9fa71706c560c06dcd84d8ca10bd2bd42cee9c2
Pavel <bavenka@gmail.com>
2017-06-27 18:23:02 +0300

    added todoInputText module

--- Frontend/src/components/header.js
+++ Frontend/src/components/header.js
@@ -1,37 +1,15 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import addTodo from '../actions/addTodo'
+import TodoTextInputContainer from '../containers/todoTextInput'
 import CompleteAll from '../containers/completeAll'
 
-let Header = ({dispatch}) => {
-
-    let textInput = null;
-
-    function handleNewTodoKeyDown(event) {
-        if (event.keyCode !== 13) {
-            return;
-        }
-        event.preventDefault();
-
-        if (textInput === null || !textInput.value.trim()) {
-            return;
-        }
-
-        dispatch(addTodo(textInput.value));
-        textInput.value = '';
-    }
+const Header = () => {
 
     return (
         <div>
             <CompleteAll/>
-            <input ref={node => {
-                textInput = node;
-                node.focus()
-            }} onKeyDown={handleNewTodoKeyDown}/>
+            <TodoTextInputContainer/>
         </div>
     )
 };
-
-Header = connect()(Header);
 
 export default Header
--- Frontend/src/components/todo.js
+++ Frontend/src/components/todo.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Complete from './complete'
 
-const Todo = ({text, onDeleteClick, onCompleteClick, completed}) => (
-    <li style={{textDecoration: completed ? 'line-through' : 'none'}}>
-        <Complete onCompleteClick={onCompleteClick} completed={completed}/>
-        {text}
-        <button onClick={onDeleteClick}>x</button>
-    </li>
-);
+class Todo extends React.Component {
+
+     render() {
+
+        return (
+            <li style={{textDecoration: this.props.completed ? 'line-through' : 'none'}}>
+                <Complete onCompleteClick={this.props.onCompleteClick} completed={this.props.completed}/>
+                {this.props.text}
+                <button onClick={this.props.onDeleteClick}>x</button>
+            </li>
+        )
+    }
+}
 
 export default Todo
