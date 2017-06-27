44a66f7c0f256a330871996cf9632b22c640e415
MIKL99999 <radykevich@gmail.com>
2017-06-27 21:02:12 +0300

    Add todos filtration.

--- Frontend/src/containers/visibleTodoList.js
+++ Frontend/src/containers/visibleTodoList.js
@@ -2,13 +2,22 @@ import {connect} from 'react-redux';
 import TodoList from '../components/todoList';
 import deleteTodo from '../actions/deleteTodo';
 
-const getTodos = (todos) => {
-    return todos;
+const getTodos = (todos, filter) => {
+    switch (filter) {
+        case 'ALL':
+            return todos;
+        case 'COMPLETED':
+            return todos.filter(t => t.completed);
+        case 'ACTIVE':
+            return todos.filter(t => !t.completed);
+        default:
+            throw new Error('Unknown filter: ' + filter)
+    }
 };
 
 const mapStateToProps = (state) => {
     return {
-        todos: getTodos(state.todos)
+        todos: getTodos(state.todos, state.visibilityFilter)
     }
 };
 
