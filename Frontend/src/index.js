5f50e602948926c84e8da875de10ab32f95278dc
Pavel <bavenka@gmail.com>
2017-06-30 18:45:31 +0300

    imported tools for routing, added default route (#/) to app component

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import {HashRouter, Route} from 'react-router-dom'
 import todoApp from './reducers/todoApp';
 import App from './components/app';
 
 const REDUX_DEVTOOLS = window.__REDUX_DEVTOOLS_EXTENSION__;
 
-let store = createStore(todoApp, REDUX_DEVTOOLS && REDUX_DEVTOOLS());
+let store = createStore(todoApp, composeWithDevTools(applyMiddleware(thunk)));
 
 render(
     <Provider store={store}>
-        <App/>
+        <HashRouter>
+            <div>
+                <Route path="/" component={App}/>
+            </div>
+        </HashRouter>
     </Provider>,
     document.getElementById('root')
 );
