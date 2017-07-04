f49d20871ebe877bc3a9d7a0669ab4247e8eae4a
MIKL99999 <radykevich@gmail.com>
2017-07-04 19:29:32 +0300

    Make Content class & add  fetchTodos on load.

--- Frontend/src/components/content.js
+++ Frontend/src/components/content.js
@@ -1,14 +1,24 @@
 import React from 'react'
+import {connect} from 'react-redux'
 import Header from '../containers/header'
 import VisibleTodoList from '../containers/visibleTodoList'
 import Footer from '../components/footer'
+import {fetchTodos} from  '../actions/fetchTodos'
 
-const Content = () => (
-    <div className="container">
-        <Header/>
-        <VisibleTodoList/>
-        <Footer/>
-    </div>
-);
+class Content extends React.Component {
 
-export default Content
+    componentWillMount() {
+        const {dispatch} = this.props;
+        dispatch(fetchTodos);
+    }
+
+    render() {
+        return <div className="container">
+            <Header/>
+            <VisibleTodoList/>
+            <Footer/>
+        </div>
+    }
+}
+
+export default connect()(Content)
