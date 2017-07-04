96d798fb3399eddc7d282d0fd54a5d571f53cabb
MIKL99999 <radykevich@gmail.com>
2017-07-04 22:24:30 +0300

    Change function to fetchTodosByUserId.

--- Frontend/src/components/content.js
+++ Frontend/src/components/content.js
@@ -3,13 +3,14 @@ import {connect} from 'react-redux'
 import Header from '../containers/header'
 import VisibleTodoList from '../containers/visibleTodoList'
 import Footer from '../components/footer'
-import {fetchTodos} from  '../actions/fetchTodos'
+import {fetchTodosByUserId} from  '../actions/fetchTodos'
 
 class Content extends React.Component {
 
     componentWillMount() {
         const {dispatch} = this.props;
-        dispatch(fetchTodos);
+        //TODO Брать id текущего пользователя из store
+        dispatch(fetchTodosByUserId('595ab20b6dd8163038f2665f'));
     }
 
     render() {
