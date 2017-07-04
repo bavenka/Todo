8e840c1e50fb93e7b3b1b7cbcedd4be72536bc71
MIKL99999 <radykevich@gmail.com>
2017-07-04 19:25:27 +0300

    Add thunk middleware.

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {compose, createStore, applyMiddleware} from 'redux';
 import {HashRouter, Route, Switch} from 'react-router-dom'
-import todoApp from './reducers/mainReducer';
+import mainReducer from './reducers/mainReducer';
 import App from './components/app';
 import SignupPage from './components/signup'
 import Content from './components/content'
+import thunk from 'redux-thunk'
+
 
 //НЕ УДАЛЯЙ БЛЯТЬ ЭТО!!!
 //ЭТО ДЛЯ ДЭБАГА!!!
 const reduxDevtoolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
-
-let store = createStore(todoApp, reduxDevtoolsExtension && reduxDevtoolsExtension());
+let store = createStore(mainReducer,
+    compose(
+        applyMiddleware(thunk),
+        reduxDevtoolsExtension && reduxDevtoolsExtension()
+    ));
 
 render(
     <Provider store={store}>
