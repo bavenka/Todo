342f61cedbf94683e79929de54a812c0688225a7
MIKL99999 <radykevich@gmail.com>
2017-07-05 19:53:56 +0300

    Refactor.

--- Frontend/src/components/content.js
+++ Frontend/src/components/content.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux'
 import Header from '../containers/header'
 import VisibleTodoList from '../containers/visibleTodoList'
 import Footer from '../components/footer'
-import {fetchTodosByUserId} from  '../actions/fetchTodos'
+import {fetchTodosByUserId} from  '../actions/fetchTodosByUserId'
 
 class Content extends React.Component {
 
