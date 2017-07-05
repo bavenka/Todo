01de90e6d135a387439e46171ec94e53c190fc03
MIKL99999 <radykevich@gmail.com>
2017-07-05 18:38:14 +0300

    Add editing todo on server.

--- Frontend/src/actions/editTodo.js
+++ Frontend/src/actions/editTodo.js
@@ -1,11 +1,34 @@
 import {EDIT_TODO} from '../constants/ActionTypes'
 
-const editTodo = (id, text) => {
+const editTodoAction = (id, text) => {
     return {
         type: EDIT_TODO,
         id,
         text
     }
 };
+
+export const editTodo = (id, text) => (dispatch) => {
+
+    return fetch(`http://127.0.0.1:3000/api/todo/`,
+        {
+            method: 'put',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                'text': text,
+                'id': id,
+            })
+        })
+        .then(response => response.json())
+        .then(todo => {
+            dispatch(editTodoAction(todo._id, todo.text))
+        })
+        .catch((err) => {
+            throw err;
+        });
+};
+
 
 export default editTodo
