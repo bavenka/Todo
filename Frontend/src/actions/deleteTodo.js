3a59a1942294c0d789912bcbca154398f8f0cfbb
MIKL99999 <radykevich@gmail.com>
2017-07-05 18:23:05 +0300

    Add deleting todo on server.

--- Frontend/src/actions/deleteTodo.js
+++ Frontend/src/actions/deleteTodo.js
@@ -1,10 +1,26 @@
 import {DELETE_TODO} from '../constants/ActionTypes'
 
-const deleteTodo = (id) => {
+const deleteTodoAction = (id) => {
     return {
         type: DELETE_TODO,
         id
     }
+};
+
+
+export const deleteTodo = (id) => (dispatch) => {
+
+    return fetch(`http://127.0.0.1:3000/api/todo/` + id,
+        {
+            method: 'delete',
+        })
+        .then(res => {
+            debugger;
+            dispatch(deleteTodoAction(id))
+        })
+        .catch((err) => {
+            throw err;
+        });
 };
 
 export default deleteTodo
