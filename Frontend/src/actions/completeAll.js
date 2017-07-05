0bab3ad603a70af6b25457938674f86ade8c81a7
MIKL99999 <radykevich@gmail.com>
2017-07-05 20:24:42 +0300

    Add completeAll function.

--- Frontend/src/actions/completeAll.js
+++ Frontend/src/actions/completeAll.js
@@ -1,9 +1,26 @@
 import {COMPLETE_ALL} from '../constants/ActionTypes'
 
-const completeAll = () => {
+const completeAllAction = () => {
     return {
         type: COMPLETE_ALL,
     }
 };
+
+//TODO передовать user_id текущего пользователя
+export const completeAll = (user_id) => (dispatch) => {
+    user_id = '595ab20b6dd8163038f2665f';
+    return fetch(`http://127.0.0.1:3000/api/todo/completeAll/` + user_id,
+        {
+            method: 'put',
+        })
+        .then(response => {
+            if (response.status === 202)
+                dispatch(completeAllAction())
+        })
+        .catch((err) => {
+            throw err;
+        });
+};
+
 
 export default completeAll
