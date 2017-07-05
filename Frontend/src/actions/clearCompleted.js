06f7cdf30c7b48a7c08c5ab789b826b8af54e9b5
MIKL99999 <radykevich@gmail.com>
2017-07-05 22:13:18 +0300

    Add clearCompleted function.

--- Frontend/src/actions/clearCompleted.js
+++ Frontend/src/actions/clearCompleted.js
@@ -1,9 +1,26 @@
 import {CLEAR_COMPLETED} from '../constants/ActionTypes'
+import {fake_user_id} from "../constants/fakeUser";
 
-const clearCompleted = () => {
+const clearCompletedAction = () => {
     return {
         type: CLEAR_COMPLETED,
     }
+};
+
+export const clearCompleted = (user_id) => (dispatch) => {
+    //TODO передовать user_id текущего пользователя
+    user_id = fake_user_id;
+    return fetch(`http://127.0.0.1:3000/api/todo/clearCompleted/` + user_id,
+        {
+            method: 'delete',
+        })
+        .then(response => {
+            if (response.status === 200)
+                dispatch(clearCompletedAction());
+        })
+        .catch((err) => {
+            throw err;
+        });
 };
 
 export default clearCompleted
