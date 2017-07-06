e872427bfd91897c6f692b9610e7b5255304c00c
MIKL99999 <radykevich@gmail.com>
2017-07-06 14:57:36 +0300

    Add reconnect to DB.

--- Backend/DB_connection.js
+++ Backend/DB_connection.js
@@ -1,14 +1,19 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/test');
-
 var DB = mongoose.connection;
+
+mongoose.connect('mongodb://localhost/test', {server: {auto_reconnect: true}});
 
 DB.on('error', function (err) {
     console.error('Connection error(DB):', err.message);
 });
-DB.once('open', function callback() {
-    console.info("Connected to DB!");
+DB.on('open', function callback() {
+    console.info("Connected to DB opened!");
+});
+
+DB.on('disconnected', function () {
+    console.log('DB disconnected!');
+    mongoose.connect('mongodb://localhost/test', {server: {auto_reconnect: true}});
 });
 
 module.exports = DB;
