a22ae5343bdf669a86d40499b21ac416f2736258
MIKL99999 <radykevich@gmail.com>
2017-07-07 21:35:52 +0300

    Migration to promises.

--- Backend/todos/controllers/putTodo.js
+++ Backend/todos/controllers/putTodo.js
@@ -1,7 +1,10 @@
 const putTodo = require('../services/putTodo');
 
-module.exports = async function (req, res, next) {
-    const todo =
-        await putTodo(req.body.id, req.body.text, req.body.user_id, req.body.completed);
-    res.status(202).json(todo);
+module.exports = function (req, res, next) {
+    putTodo(req.body.id, req.body.text, req.body.user_id, req.body.completed).then(todo => {
+            res.status(202).json(todo);
+        }
+    ).catch(err => {
+        res.status(500).send(err);
+    });
 };
--- Backend/todos/services/putTodo.js
+++ Backend/todos/services/putTodo.js
@@ -1,26 +1,17 @@
 const TODO = require('../models/todo');
 
 module.exports = function (id, text, userId, completed) {
-    return TODO.findById(id, function (err, todo) {
-        if (err)
-            return err;
-        if (todo !== null) {
-            if (text) {
-                todo.text = text;
-            }
-            if (userId) {
-                todo.user = userId;
-            }
-            if (completed) {
-                todo.completed = completed;
-            }
 
-            return todo.save(function (err, todo) {
-                if (err)
-                    return err;
-                return todo;//TODO Спросить почему не работает!
-            });
-        }
-    });
+    let newTodo = {};
+    if (text) {
+        newTodo.text = text;
+    }
+    if (userId) {
+        newTodo.user = userId;
+    }
+    if (completed) {
+        newTodo.completed = completed;
+    }
+    return TODO.findByIdAndUpdate(id, newTodo, {new: true}).exec();
 };
 
