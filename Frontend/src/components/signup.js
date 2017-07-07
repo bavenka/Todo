88f1349b21354dd70e65efec5ba1c87fa85ab04e
Pavel <bavenka@gmail.com>
2017-07-08 02:11:49 +0300

    small fixes

--- Frontend/src/components/signup.js
+++ Frontend/src/components/signup.js
@@ -1,26 +1,24 @@
 import React, {PropTypes} from 'react';
 import {connect} from 'react-redux'
-import userSignup from '../actions/userSignup'
 import addFlashMessage from '../actions/addFlashMessage'
 
 
 import SignupForm from './signupForm.js'
 
-const SignupPage = ({userSignup, addFlashMessage}) => {
+const SignupPage = ({addFlashMessage}) => {
 
     return (
         <div className="row">
             <div className="col-md-4 col-md-offset-4">
-                <SignupForm userSignup={userSignup}
-                            addFlashMessage={addFlashMessage}/>
+                <SignupForm
+                    addFlashMessage={addFlashMessage}/>
             </div>
         </div>
     )
 };
 
 SignupPage.propTypes = {
-    userSignup: PropTypes.func.isRequired,
     addFlashMessage: PropTypes.func.isRequired
 };
 
-export default connect(null, {userSignup, addFlashMessage})(SignupPage);
+export default connect(null, {addFlashMessage})(SignupPage);
