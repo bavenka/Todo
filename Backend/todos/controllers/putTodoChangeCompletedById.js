d8f2083d43d7e0b233c141a5b8a3ca50c073935f
MIKL99999 <radykevich@gmail.com>
2017-07-09 18:23:40 +0300

    Add console log.

--- Backend/todos/controllers/putTodoChangeCompletedById.js
+++ Backend/todos/controllers/putTodoChangeCompletedById.js
@@ -4,6 +4,7 @@ export default async function (req, res, next) {
     putTodoChangeCompletedById(req.params.id).then((todo) => {
         res.status(202).json(todo);
     }).catch(err => {
+        console.log(err);
         res.status(500).send(err);
     });
 };
