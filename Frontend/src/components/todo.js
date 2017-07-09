69b1dd683129ae67841be97106b78706d03b92a7
MIKL99999 <radykevich@gmail.com>
2017-07-09 03:49:47 +0300

    Change prop type.

--- Frontend/src/components/todo.js
+++ Frontend/src/components/todo.js
@@ -37,7 +37,8 @@ class Todo extends React.Component {
             element = (
                 <div>
                     <Complete onCompleteClick={this.props.onCompleteClick} completed={this.props.completed}/>
-                    <label onDoubleClick={this.onDoubleClick} style={{textDecoration: this.props.completed ? 'line-through' : 'none'}}>
+                    <label onDoubleClick={this.onDoubleClick}
+                           style={{textDecoration: this.props.completed ? 'line-through' : 'none'}}>
                         {this.props.text}
                     </label>
                     <button onClick={this.props.onDeleteClick}>x</button>
@@ -55,7 +56,7 @@ class Todo extends React.Component {
 }
 
 Todo.propTypes = {
-    id: PropTypes.number.isRequired,
+    id: PropTypes.string.isRequired,
     text: PropTypes.string.isRequired,
     completed: PropTypes.bool.isRequired,
     onEditClick: PropTypes.func.isRequired,
