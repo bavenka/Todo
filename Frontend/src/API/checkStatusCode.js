1e2940bd50f9c3478eb463267d118d38e51a3c3c
MIKL99999 <radykevich@gmail.com>
2017-07-10 11:11:05 +0300

    Change throw object.

--- Frontend/src/API/checkStatusCode.js
+++ Frontend/src/API/checkStatusCode.js
@@ -2,5 +2,5 @@ export default function checkStatusCode(response) {
     if (response.status >= 200 && response.status <= 399)
         return response;
     else
-        throw response.error;
+        throw response;
 }
