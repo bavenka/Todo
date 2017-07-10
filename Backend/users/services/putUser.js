0dcbd90f01e6ce9940c845a5696b9e6d6c10bdce
MIKL99999 <radykevich@gmail.com>
2017-07-10 12:10:34 +0300

    Refactor.

--- Backend/users/services/putUser.js
+++ Backend/users/services/putUser.js
@@ -1,12 +1,15 @@
 import User from '../models/user';
 
 export default function (id, username, email, password) {
-    return User.findById(id, function (err, user) {
-        if (err)
-            throw err;
-        user.username = username;
-        user.email = email;
-        user.password = password;
-        return user.save();
-    });
+    let newUser = {};
+    if (username) {
+        newUser.username = username;
+    }
+    if (email) {
+        newUser.email = email;
+    }
+    if (password) {
+        newUser.password = password;
+    }
+    return User.findByIdAndUpdate(id, newUser, {new: true}).exec();
 };
