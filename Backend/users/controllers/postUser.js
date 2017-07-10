997b7588d0c41cfbafdeff2def7950bc5fc1a80e
MIKL99999 <radykevich@gmail.com>
2017-07-10 23:05:04 +0300

    Change error code.

--- Backend/users/controllers/postUser.js
+++ Backend/users/controllers/postUser.js
@@ -4,6 +4,6 @@ export default function (req, res, next) {
     postUser(req.body.username, req.body.email, req.body.password).then(user => {
         res.status(201).json(user);
     }).catch(err => {
-        res.status(500).send(err);
+        res.status(409).send(err);
     });
 };
