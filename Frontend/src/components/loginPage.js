84c4f62346b834df06944ead3ef2f59bec019c4a
Pavel <bavenka@gmail.com>
2017-07-11 00:27:43 +0300

    login page functionality refactoring

--- Frontend/src/components/loginPage.js
+++ Frontend/src/components/loginPage.js
@@ -1,14 +1,16 @@
 import React, {PropTypes} from 'react';
 import LoginForm from './loginForm.js'
+import loginUser from '../actions/loginUser'
+import {connect} from 'react-redux'
 
-const LoginPage = ({login}) => {
+const LoginPage = ({loginUser}) => {
 
     return (
         <div className="row">
             <div className="col-md-4 col-md-offset-4">
-                <LoginForm onLogin={login}/>
+                <LoginForm onLogin={loginUser}/>
             </div>
         </div>
     )
 };
-export default LoginPage;
+export default connect(null, {loginUser})(LoginPage);
