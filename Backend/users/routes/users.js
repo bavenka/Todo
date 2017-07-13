6673713d50a749f55705a299251933497b54ed6f
MIKL <radykevich@gmail.com>
2017-07-13 17:41:47 +0300

    Separate authorization to different levels.

--- Backend/users/routes/users.js
+++ Backend/users/routes/users.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import validate from 'express-validation'
-import jwt from 'jsonwebtoken'
 import {
     getUserController,
     getUsersController,
@@ -9,16 +8,16 @@ import {
     postUserController,
     putUserController,
     deleteUserByIdController,
-    deleteUserByEmailController
+    deleteUserByEmailController,
+    authorizationController
 } from '../controllers/index'
+
 import {
     postUserValidator,
     putUserValidator
 } from '../validators/index'
 
-import User from '../models/user'
 import Authorization from '../../middleware/authorization'
-import {Secret} from '../../constants/index'
 
 let router = express.Router();
 
@@ -33,50 +32,7 @@ router.use(Authorization.unless({
 
 //TODO Добавить валидатор!
 //TODO Разнести по уровням.
-router.post('/authorization', function (req, res, next) {
-        let searchParam;
-        const email = req.body.email;
-        const username = req.body.username;
-        if (email) {
-            searchParam = {email: email};
-        } else if (username) {
-            searchParam = {username: username};
-        }
-
-        User.findOne(searchParam, function (err, user) {
-            if (err)
-                throw err;
-
-            if (!user) {
-                res.status(404).json({success: false, message: 'Authentication failed. User not found.'});
-            } else if (user) {
-
-                if (user.password !== req.body.password) {
-                    res.status(409).json({success: false, message: 'Authentication failed. Wrong password.'});
-                } else {
-
-                    const pureUser = {
-                        username: user.username,
-                        email: user.email,
-                        id: user._id
-                    };
-
-                    const token = jwt.sign(pureUser, Secret, {
-                        expiresIn: 86400 // expires in 24 hours
-                    });
-
-                    res.status(200).json({
-                        success: true,
-                        message: 'Enjoy your token!',
-                        token: token
-                    });
-                }
-
-            }
-
-        });
-    }
-);
+router.post('/authorization', authorizationController);
 
 // Скрыт для безопасности (любой юзер имеет возможность получить всех пользователей)
 //router.get('/all', getUsersController);
