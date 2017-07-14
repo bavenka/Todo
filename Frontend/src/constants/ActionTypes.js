8d0c77ca66b40ebc34e2ff6eb0a81db3837f37f0
MIKL <radykevich@gmail.com>
2017-07-14 12:33:47 +0300

    Refactor.

--- Frontend/src/constants/ActionTypes.js
+++ Frontend/src/constants/ActionTypes.js
@@ -8,7 +8,6 @@ export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
 export const CREATE_USER = 'CREATE_USER';
 export const ERROR_GENERATED = 'ERROR_GENERATED';
 //API
-export const REQUEST_TODOS = 'REQUEST_TODOS';
 export const RECEIVE_TODOS = 'RECEIVE_TODOS';
 export const SERVER_URL = 'http://127.0.0.1:3000/api';
 //Sign UP
