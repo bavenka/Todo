7d7e2a39d8e3f878d21351e68cc982ccb21b9bd7
Pavel_Kavalchuk <bavenka@gmail.com>
2017-07-28 17:43:05 +0300

    improved validators

--- Frontend/src/validators/validate/validateSignupForm.js
+++ Frontend/src/validators/validate/validateSignupForm.js
@@ -1,20 +1,21 @@
 const validate = (values) => {
     let errors = {};
 
-    const emptyFieldMessage = "This field is required.";
+    const requiredFields = [
+        'username',
+        'email',
+        'password',
+    ];
 
-    if (!values.username) {
-        errors.username = emptyFieldMessage;
+    requiredFields.forEach(field => {
+        if (!values[field]) {
+            errors[field] = 'This field is required.'
+        }
+    });
+
+    if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email address'
     }
-        if (!values.email) {
-            errors.email = emptyFieldMessage
-        }
-        else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-            errors.email = 'Invalid email address'
-        }
-        if (!values.password) {
-            errors.password = emptyFieldMessage
-        }
     return errors;
 };
 export default validate;
